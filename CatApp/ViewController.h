1ae66c0eb2b894e28a932bcb8fd054187cdf991c
Dionísio <dionisio@malteso.com>
2022-04-27 00:10:29 -0400

    Cat sound effect on Press

--- CatApp/ViewController.h
+++ CatApp/ViewController.h
@@ -6,8 +6,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AudioToolbox/AudioToolbox.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    
+    SystemSoundID soundID;
+    
+}
 
 @property (weak, nonatomic) IBOutlet UILabel *label;
 - (IBAction)cat:(id)sender;
--- CatApp/ViewController.m
+++ CatApp/ViewController.m
@@ -17,12 +17,26 @@
     [super viewDidLoad];
 
     self.label.hidden = YES;
+    
+    NSURL *soundURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource: @"Cat" ofType: @"wav"]];
+    
+    AudioServicesCreateSystemSoundID((__bridge CFURLRef) soundURL, &soundID);
 }
 
 
 - (IBAction)cat:(id)sender {
     
     self.label.hidden = NO;
+     
+    AudioServicesPlaySystemSound(soundID);
+    
+    [NSTimer scheduledTimerWithTimeInterval:1.3 target:self selector:@selector(hideLabel) userInfo:nil repeats:NO];
     
 }
+
+
+- (void)hideLabel {
+    self.label.hidden = YES;
+}
+
 @end
